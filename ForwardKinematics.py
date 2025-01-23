445d6358e170f3603dbd25612928325cc80bb039
tomoya-y <yamanokuchi.tomoya.ys9@is.naist.jp>
2025-01-23 12:52:41 +0900

    add test code main block

--- ForwardKinematics.py
+++ ForwardKinematics.py
@@ -55,3 +55,33 @@ class ForwardKinematics:
 
 
 
+if __name__ == '__main__':
+    forward = ForwardKinematics()
+
+    joint_position = np.array(
+        [
+            [0.0, 0.0, 0.0],
+            [0.0, np.pi*0.5, 0.0],
+            [0.0, -np.pi*0.5, 0.0],
+            [0.0, 0.0, np.pi*0.5],
+            [0.0, 0.0, -np.pi*0.5],
+            [np.pi*0.5, 0.0, 0.0],
+            # [-np.pi*0.5, 0.0, 0.0], # ダメな角度
+            [forward.kinematics.theta0_lb+0.01, 0.0, 0.0], # ダメな角度
+        ]
+    )
+
+    joint_position = np.array(
+        [
+            [0.0, -np.pi*0.5 , np.pi*0.5],
+        ]
+    )
+    endeffector_position =  forward.calc_1claw(joint_position)
+
+    joint_position = np.array(
+        [
+            [0.0, -np.pi*0.5 , np.pi*0.5, 0.0, -np.pi*0.5 , np.pi*0.5, 0.0, -np.pi*0.5 , np.pi*0.5],
+        ]
+    )
+    endeffector_position =  forward.calc(joint_position)
+    print(endeffector_position)
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# ForwardKinematics and InverseKinematics
+# robel_dclaw_kinematics
+
+
+## "__main__"ブロックのテストコードの実行方法
+
+
+### ForwardKinematics
+```
+python3.7 -m robel_dclaw_kinematics.ForwardKinematics
+```
+
+### InverseKinematics
+```
+python3.7 -m robel_dclaw_kinematics.InverseKinematics
+```
+
+
